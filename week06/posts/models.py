dd5f158020fa08317c69f31c6e25723bc18d4c5a
y11n <samtori3849@gmail.com>
2024-05-12 00:08:22 +0900

    6주차 과제: Comment 모델 추가

--- week06/posts/models.py
+++ week06/posts/models.py
@@ -7,3 +7,11 @@ class Post (models.Model):
     contentn = models.TextField('내용')
     created_at = models.DateTimeField('작성일')
     view_count = models.IntegerField('조회수', default=0)
+
+class Comment (models.Model):
+    image = models.ImageField(verbose_name='이미지')
+    nickname = models.CharField('닉네임', max_length=10)
+    content = models.TextField('내용')
+    time = models.TimeField('작성 시간')
+    likes = models.IntegerField('좋아요수', default=0)
+    dislikes = models.IntegerField('싫어요수', default=0)
