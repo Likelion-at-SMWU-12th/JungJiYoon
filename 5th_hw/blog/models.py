b268c26b112c98e938270a09ebf69547068d68ae
y11n <samtori3849@gmail.com>
2024-07-15 14:47:29 +0900

    Post/Comment 모델 추가

--- 5th_hw/blog/models.py
+++ 5th_hw/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now=True)
