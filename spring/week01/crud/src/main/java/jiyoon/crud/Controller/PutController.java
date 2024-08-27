ac95768f27f975c42753d81df3739682290e0fe3
y11n <samtori3849@gmail.com>
2024-08-27 19:49:07 +0900

    [FEAT] 3주차 세션 실습

--- spring/week01/crud/src/main/java/jiyoon/crud/Controller/PutController.java
+++ spring/week01/crud/src/main/java/jiyoon/crud/Controller/PutController.java
@@ -1,23 +1,23 @@
-package jiyoon.crud.Controller;
-
-import jiyoon.crud.Dto.MemberDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-
-@RestController
-@RequestMapping("/api/v1/put-api")
-public class PutController {
-
-    @PutMapping(value = "/member3")
-    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto) {
-        return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(memberDto);
-    }
-}
+//package jiyoon.crud.Controller;
+//
+//import jiyoon.crud.Dto.MemberDto;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/api/v1/put-api")
+//public class PutController {
+//
+//    @PutMapping(value = "/member3")
+//    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto) {
+//        return ResponseEntity
+//                .status(HttpStatus.ACCEPTED)
+//                .body(memberDto);
+//    }
+//}
