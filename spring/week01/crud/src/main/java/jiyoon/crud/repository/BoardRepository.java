0686e1ea7684bf6f8a4a033db465d4ece6fe93a1
정지윤 <111862541+y11n@users.noreply.github.com>
2024-09-08 14:36:49 +0900

    Delete spring/week01/crud directory

--- spring/week01/crud/src/main/java/jiyoon/crud/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package jiyoon.crud.repository;
-
-import jiyoon.crud.entity.BoardEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BoardRepository extends CrudRepository<BoardEntity, Long> {
-}
--- spring/week01/crud/src/main/java/jiyoon/crud/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package jiyoon.crud.repository;
-
-import jiyoon.crud.entity.PostEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<PostEntity, Long> {
-}
