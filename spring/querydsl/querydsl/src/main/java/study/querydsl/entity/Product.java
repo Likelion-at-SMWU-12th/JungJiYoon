77cf5942e66ded987e0cd8650c06fd78c293d604
y11n <samtori3849@gmail.com>
2024-09-08 15:33:13 +0900

    [FEAT] popularity 필드 추가

--- spring/querydsl/querydsl/src/main/java/study/querydsl/entity/Product.java
+++ spring/querydsl/querydsl/src/main/java/study/querydsl/entity/Product.java
@@ -21,11 +21,13 @@ public class Product {
     private String name;
     private int price;
     private int stock;
+    private int popularity;
 
-    public Product(String name, int price, int stock){
+    public Product(String name, int price, int stock, int popularity){
         this.name = name;
         this.price = price;
         this.stock = stock;
+        this.popularity = popularity;
     }
 
 }
